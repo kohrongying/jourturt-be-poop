a84b92d85f98a0a84bc83005ba47197348129e3e
rongying <kohrongying@gmail.com>
2022-05-23 18:07:01 +0800

    Add parser.

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -3,12 +3,16 @@ import { getItem } from "../services/ddbService.js";
 
 const TABLE_NAME = "ddb-last-poop-dev-users"
 
+const parseItem = (item) => {
+    return new User(item.Id, item.Name, item.Email)
+}
+
 const UserRepository = {
     get: async(email) => {
         const res = await getItem(TABLE_NAME, {
             Email: email,
         })
-        return new User(res.Item.Id, res.Item.Name, res.Item.Email)
+        return parseItem(res.Item)
     },
 }
 
