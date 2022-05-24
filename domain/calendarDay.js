f9110b4f7fe9149456f8a84de0b947515c0d3451
rongying <kohrongying@gmail.com>
2022-05-24 18:06:34 +0800

    Add utc and timezone to dayjs to fix build errors.

--- domain/calendarDay.js
+++ domain/calendarDay.js
@@ -1,5 +1,10 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault("Asia/Singapore")
 class CalendarDay {
     constructor(timestamp) {
         this.timestamp = timestamp
