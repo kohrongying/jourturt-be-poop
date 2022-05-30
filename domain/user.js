f55ab7b5708fcf01f4177f3fa857ebcc97429e7c
rongying <kohrongying@gmail.com>
2022-05-30 23:25:01 +0800

    Add impl and test for getting event logs of user.

--- domain/user.js
+++ domain/user.js
@@ -8,7 +8,17 @@ class User {
     }
 
     async getEvents() {
-        return await EventRepository.query(this.id)
+        return await EventRepository.findByUser(this.id)
+    }
+
+    async getEventLogs(startTime, endTime) {
+        const events = await this.getEvents()
+        let eventLogs = []
+        for (let event of events)  {
+            const logs = await event.getLogsByDay(startTime, endTime)
+            eventLogs = eventLogs.concat(logs)
+        }
+        return eventLogs
     }
 }
 
--- tests/domain/user.test.js
+++ tests/domain/user.test.js
@@ -21,3 +21,37 @@ t.test('get events', async t => {
     t.strictSame(found, [event])
     t.end()
 })
+
+t.test('get event logs', async t => {
+    // given
+    const { id, user } = generateUser()
+    const { id: eventId, event } = generateEvent()
+    const { id: eventId2, event: event2 } = generateEvent()
+    
+    const { eventLog } = generateEventLog()
+    const { eventLog: eventLog2 } = generateEventLog()
+
+    const startTime = 1653235200000
+    const endTime = 1653321599999
+    
+    // mock 
+    const eventRepositoryMock = Sinon.mock(EventRepository)
+    eventRepositoryMock.expects('findByUser').once().withArgs(id)
+        .returns([event, event2])
+    const eventLogRepositoryMock = Sinon.mock(EventLogRepository)
+    eventLogRepositoryMock.expects('query').once().withArgs(eventId, startTime, endTime)
+        .returns([eventLog])
+    eventLogRepositoryMock.expects('query').once().withArgs(eventId2, startTime, endTime)
+        .returns([eventLog2])
+    
+        // when
+    const found = await user.getEventLogs(startTime, endTime)
+
+    // then
+    eventRepositoryMock.verify()
+    eventRepositoryMock.restore()
+    eventLogRepositoryMock.verify()
+    eventLogRepositoryMock.restore()
+    t.strictSame(found, [eventLog, eventLog2])
+    t.end()
+})
